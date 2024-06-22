9cf78a6bde845fda61159b273ae8c0dc2a4de02d
Soh Hong Yu <sohhongyu@gmail.com>
2024-06-23 03:44:11 +0800

    Added Shirt + Pants

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Group, Image as KonvaImage } from "react-konva";
+import clothesImageSrc from "@/assets/character/clothes/basic.png";
+import pantsImageSrc from "@/assets/character/clothes/pants.png";
+import hairImageSrc from "@/assets/character/hair/gentleman.png";
 import useImage from "use-image";
 
 interface PlayerProps {
@@ -20,6 +23,9 @@ const Player: React.FC<PlayerProps> = ({
   playerFrameCoords,
 }) => {
   const [image] = useImage(playerImageSrc);
+  const [clothesImage] = useImage(clothesImageSrc);
+  const [hairImage] = useImage(hairImageSrc);
+  const [pantsImage] = useImage(pantsImageSrc);
 
   return (
     <Group x={x} y={y} scaleX={scale} scaleY={scale}>
@@ -31,6 +37,45 @@ const Player: React.FC<PlayerProps> = ({
         image={image}
         crop={{
           x: playerFrameCoords.x * tileSize,
+          y: playerFrameCoords.y * tileSize,
+          width: tileSize,
+          height: tileSize,
+        }}
+      />
+      <KonvaImage
+        x={0}
+        y={0}
+        width={tileSize}
+        height={tileSize}
+        image={clothesImage}
+        crop={{
+          x: playerFrameCoords.x * tileSize,
+          y: playerFrameCoords.y * tileSize,
+          width: tileSize,
+          height: tileSize,
+        }}
+      />
+      <KonvaImage
+        x={0}
+        y={0}
+        width={tileSize}
+        height={tileSize}
+        image={hairImage}
+        crop={{
+          x: playerFrameCoords.x * tileSize,
+          y: playerFrameCoords.y * tileSize,
+          width: tileSize,
+          height: tileSize,
+        }}
+      />
+      <KonvaImage
+        x={0}
+        y={0}
+        width={tileSize}
+        height={tileSize}
+        image={pantsImage}
+        crop={{
+          x: playerFrameCoords.x * tileSize + 6 * tileSize * 8,
           y: playerFrameCoords.y * tileSize,
           width: tileSize,
           height: tileSize,
