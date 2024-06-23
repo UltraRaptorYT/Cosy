a555bdc2cb83855eb8cdbf3918f6ce9fc57061ef
Soh Hong Yu <sohhongyu@gmail.com>
2024-06-23 11:46:13 +0800

    Added Speech Recog

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,85 @@
-import { useEffect, useContext, useState } from "react";
+import {
+  useEffect,
+  useContext,
+  useState,
+  useRef,
+  useCallback,
+  useMemo,
+} from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "@/context/UserContext";
 import Game from "@/components/Game";
-// import Peer from "simple-peer";
+import "regenerator-runtime/runtime";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 export default function Home() {
   const userContext = useContext(UserContext);
   const navigate = useNavigate();
-  const [_, setStream] = useState<MediaStream>();
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const lastProcessedTranscript = useRef<string>("");
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
   useEffect(() => {
     const user = sessionStorage.getItem("user");
     if (user) {
       userContext?.setUser(JSON.parse(user));
-      console.log(JSON.parse(user));
-      return;
-    }
-    if (!userContext || !userContext?.user) {
+    } else if (!userContext?.user) {
       navigate(`/login`);
     }
-    console.log(userContext?.user);
-  }, []);
+  }, [userContext, navigate]);
 
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ video: false, audio: true })
       .then((currentStream) => {
         setStream(currentStream);
+        SpeechRecognition.startListening({ continuous: true,language: 'en-GB' });
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices:", error);
       });
   }, []);
 
+  const memoizedTranscript = useMemo(
+    () => transcript.toLowerCase(),
+    [transcript]
+  );
+
+  const handleTranscript = useCallback(() => {
+    if (
+      memoizedTranscript.includes("open cosy") &&
+      lastProcessedTranscript.current !== memoizedTranscript
+    ) {
+      alert("Command recognized: Open Cosy");
+      lastProcessedTranscript.current = memoizedTranscript;
+      resetTranscript();
+    }
+  }, [memoizedTranscript, resetTranscript]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      handleTranscript();
+    }, 1000); // Debounce time in milliseconds
+
+    return () => clearTimeout(handler);
+  }, [memoizedTranscript, handleTranscript]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
     <div className="h-full flex items-center justify-center">
-      <Game user={userContext?.user}></Game>
+      <Game user={userContext?.user} />
+      <p>Listening: {listening ? "Yes" : "No"}</p>
+      <p>Recognized Word: {transcript}</p>
     </div>
   );
 }
